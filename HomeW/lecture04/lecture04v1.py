437d496c5c1a010323b9171346c6f3d198637100
Aleks-du <71539766+Aleks-du@users.noreply.github.com>
2021-03-18 10:42:18 +0700

    Update lecture04v1.py

--- HomeW/lecture04/lecture04v1.py
+++ HomeW/lecture04/lecture04v1.py
@@ -1,7 +1,14 @@
 #Определить какой из трех классификаторов (kNN, наивный Байес, решающее дерево) лучший в метрике accuracy.
 #наивный Байес
+import numpy as np
+import pandas as pd
+import sklearn
 from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
+
+data = pd.read_csv("mlbootcamp5_train.csv", 
+                 sep=";", 
+                 index_col="id")
 
 train = data[["age", "weight", "height"]]
 target = data["cardio"]
