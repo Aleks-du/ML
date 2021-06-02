29fdea9f5a9d961c6a57a38d834ea697bf05d10e
Aleks-du <71539766+Aleks-du@users.noreply.github.com>
2021-06-02 16:05:55 +0700

    Update Past 1.py

--- HomeW/House Price/Past 1.py
+++ HomeW/House Price/Past 1.py
@@ -183,3 +183,48 @@ y = X.pop("SalePrice")
 
 baseline_score = score_dataset(X, y)
 print(f"Baseline score: {baseline_score:.5f} RMSLE")
+
+
+
+# Такого результата нам мало.
+# Вычисляем оценку "полезности"
+# Значения должны быть целочисленными
+def make_mi_scores(X, y):
+    X = X.copy()
+    for colname in X.select_dtypes(["object", "category"]):
+        X[colname], _ = X[colname].factorize()
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X.dtypes]
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+    
+# Проверяем
+X = df_train.copy()
+y = X.pop("SalePrice")
+
+mi_scores = make_mi_scores(X, y)
+
+
+# Уберем не информативные значения и проверяем
+def drop_uninformative(df, mi_scores):
+    return df.loc[:, mi_scores > 0.0]
+
+X = df_train.copy()
+y = X.pop("SalePrice")
+X = drop_uninformative(X, mi_scores)
+
+score_dataset(X, y)
+
+
+
+mi_scores
